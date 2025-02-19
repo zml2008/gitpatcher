5fbe0211d0d618239a22bb5c9b30f662d6028832
zml <zml@stellardrift.ca>
2025-02-18 20:58:02 -0800

    fix: correct property annotations (#30)
    FindGitTask was incorrectly updated

--- src/main/groovy/ca/stellardrift/gitpatcher/task/FindGitTask.groovy
+++ src/main/groovy/ca/stellardrift/gitpatcher/task/FindGitTask.groovy
@@ -25,14 +25,12 @@ package ca.stellardrift.gitpatcher.task
 import ca.stellardrift.gitpatcher.Git
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.file.ProjectLayout
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
-
-import javax.inject.Inject
 
 abstract class FindGitTask extends DefaultTask {
 
-    @Inject
+    @Input
     protected abstract DirectoryProperty getRootDir();
 
     FindGitTask() {
